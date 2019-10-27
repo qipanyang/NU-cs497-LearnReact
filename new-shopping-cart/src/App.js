fac2ecb2a30242eeb399cecf77a19f5214f993ec
Panyang Qi <qipanyang@126.com>
2019-10-27 13:39:57 -0500

    Public site created

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -2,37 +2,66 @@ import 'rbx/index.css';
 import {Title} from 'rbx';
 import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Container } from '@material-ui/core';
+import { Container, AppBar } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Slide from '@material-ui/core/Slide';
+import PropTypes from 'prop-types';
 
 
-import {FilterableProductTable} from './components/FilterProductTable';
 
 
 
-
-
-const App = () => {
-  const [data, setData] = useState({});
-  const products = Object.values(data);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await fetch('./data/products.json');
-      const json = await response.json();
-      setData(json);
-    };
-    fetchProducts();
-  }, []);
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
 
   return (
-    // <ul>
-    //   {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    // </ul>
-    <Container>
-    <Title>
-      Buy Your T-shirt!
-    </Title>
-    <FilterableProductTable products = {products} />
-    </Container>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+const App = (props) => {
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar className = {{alignItems: 'right',}} position="fixed">
+          <Toolbar>
+            <Typography variant="h5" align="right">Panyang's shopping cart</Typography>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+      {/* <Container> */}
+        {/* <Box my={2}>
+          {[...new Array(12)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box>
+      </Container> */}
+    </React.Fragment>
   );
 };
 
